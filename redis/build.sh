33a72cc950ff845b63885dbb5c5f6bca641a9742
Marcos Lorenzo <mlorenzo@stratio.com>
2020-03-08 15:52:10 +0100

    Poor man's redis build using docker

---
+++ redis/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+VERSION="5.0.7"
+REDIS_VERSION="redis-$VERSION"
+REDIS_FILE="$REDIS_VERSION.tar.gz"
+REDIS_URL="http://download.redis.io/releases/$REDIS_FILE"
+if [[ ! -e "$REDIS_FILE" ]]; then
+  wget "$REDIS_URL"
+  tar xvfz "$REDIS_FILE"
+fi
+docker run -i --entrypoint /bin/bash --rm -v "$PWD/$REDIS_VERSION":"/target" centos:7 << COMMANDS
+yum install -y make gcc kernel-headers jemalloc-devel
+cd /target
+make
+chown -R $UID:${GROUPS[0]} /target
+COMMANDS
+mkdir -p bin
+cp "$PWD/$REDIS_VERSION"/src/redis-{server,benchmark,cli,check-rdb,check-aof} bin/
